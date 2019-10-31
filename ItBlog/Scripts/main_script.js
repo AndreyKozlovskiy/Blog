99b2060efded185297205ced62137f778a10bc29
vadim <vadikoff57@gmail.com>
2019-11-01 01:15:17 +0300

    refactor: add front

--- ItBlog/Scripts/main_script.js
+++ ItBlog/Scripts/main_script.js
@@ -1,12 +1,17 @@
 var div = document.getElementById("all");
 var items = document.getElementsByClassName("on_item");
 var h = 1500;
-
-    if (items.length > 4) {
-        div.style.height = h + "px";
-        if ((items.length > 6) & (items.length % 2 == 1)) {
-        h += 500;
-        div.style.height = h + "px";
-    }
+//dinamic main block
+//while (true) {
+//    if (items.length > 4 & items.length % 2 == 0) {
+//        h += 500;
+//        div.style.height = h + "px";
+//    }
+//}
+let nav = document.getElementsByClassName("nav_ul");
+console.log(nav);
+let elem = document.getElementsByClassName("navbar-right");
+if (elem) {
+    //nav[0].style.width = 140 + "%";
+    elem[0].style.marginLeft = 0 +"%";
 }
-console.log(items.length);
